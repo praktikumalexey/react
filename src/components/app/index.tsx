75356454859d5ae165a2a5c5d8de0365dee90396
Pan Alexey <pan-alexey@ozon.ru>
2021-06-27 15:45:54 +0300

    Init webpack config

---
+++ src/components/app/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "~/static/logo.svg";
+import styles from "./App.module.css";
+
+function App(): React.ReactElement {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles.AppLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
